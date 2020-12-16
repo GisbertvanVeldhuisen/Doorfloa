48b13185fc5fb5c27dda889d1ab9a30e8f20f9d8
GisbertvanVeldhuisen <gisbertvveldhuisen@gmail.com>
2020-12-16 11:26:08 +0100

    new

--- public/js/app.js
+++ public/js/app.js
@@ -19384,8 +19384,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\xampp\htdocs\doorflora\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\xampp\htdocs\doorflora\resources\sass\main.scss */"./resources/sass/main.scss");
+__webpack_require__(/*! C:\xampp\htdocs\Doorfloa\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\xampp\htdocs\Doorfloa\resources\sass\main.scss */"./resources/sass/main.scss");
 
 
 /***/ })
