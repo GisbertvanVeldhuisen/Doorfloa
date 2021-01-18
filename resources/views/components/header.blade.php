6432df4ebe8aff1d107cc128c8dd7917b8e40638
GisbertvanVeldhuisen <gisbertvveldhuisen@gmail.com>
2021-01-18 15:59:19 +0100

    edit post

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,18 +1,19 @@
 <header id="header" class="menu">
     <div class="container">
-        <div class="row">
-            <div class="col-1">
-                <a class="menu-item icon home" href="{{ url('/') }}">
-                </a>
-            </div>
+        <div class="logo">
+            <a class="menu-item icon home" href="{{ url('/') }}">
+            </a>
+        </div>
+        <script>
 
-            <div class="col-11">
-                <a class="menu-item icon search" href="{{ url('#') }}"></a>
-                <a class="menu-item icon instagram" href="{{ url('#') }}"></a>
-                <a class="menu-item icon pintrest" href="{{ url('#') }}"></a>
-                <a class="menu-item icon facebook" href="{{ url('#') }}"></a>
-                <a class="menu-item icon mail" href="{{ url('#') }}"></a>
-            </div>
+        </script>
+
+        <div class="menu">
+            <a class="menu-item icon search" href="{{ url('#') }}"></a>
+            <a class="menu-item icon instagram" href="{{ url('#') }}"></a>
+            <a class="menu-item icon pintrest" href="{{ url('#') }}"></a>
+            <a class="menu-item icon facebook" href="{{ url('#') }}"></a>
+            <a class="menu-item icon mail" href="{{ url('#') }}"></a>
         </div>
     </div>
 </header>
